21fb33fb05d33a67b037bcf0f8298f328e1cf548
Huy Lưu <huylv@hopnhat.vn>
2020-08-19 16:54:49 +0700

    fix svg camelCase

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,66 +1,68 @@
 import Header from '../../components/Header'
 import { Container } from '@material-ui/core';
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Icon from './icon'
 
-function ProcessKey(key) {
+function ProcessMetaKey(key) {
   var str = key;
   var res = str.split(" ");
   console.log('dev', res.toString())
 }
 
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <div key={number.toString()}>
+      {number}
+    </div>
+  );
+  return (
+    <div >
+      <p>{listItems}</p>
+    </div>
+  );
+}
+
 export default function AboutPage(props) {
+
+  const { asPath } = useRouter()
+
+  console.log('propsx', asPath)
 
   const { tag, category, title, content } = props
 
-  ProcessKey(title);
-
-  console.log('propsxxx', tag)
-  const obj = props;
-  function createMarkup() {
-    return { __html: obj.content };
-  }
-
-  function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-      <div key={number.toString()}>
-        {number}
-      </div>
-    );
-    // let tag = numbers.map((item) => item)
-    // console.log('taggg', tag)
-    return (
-      <p>{listItems}</p>
-    );
-  }
+  ProcessMetaKey(title);
 
   return (
     <>
       <Head>
-        <title>{obj.title}</title>
+        <title>{title}</title>
       </Head>
       <Header />
       <Container maxWidth='md' style={{ backgroundColor: '#fff' }} >
         <div>
 
-          <h1 style={{ fontFamily: 'Oswald', fontSize: "2.5em", fontWeight: 'bold' }} >{obj.title}</h1>
-          <div>
-            <Icon />
+          <h1 style={{ fontFamily: 'Oswald', fontSize: "2.5em", fontWeight: 'bold' }} >{title}</h1>
+
+          <div style={{ paddingLeft: 15, }}>
+            {/* <Icon /> */}
+            <div>
+              <img style={{ borderRadius: 50, verticalAlign: 'center' }} alt="Kriss" className="icon" src="https://miro.medium.com/fit/c/96/96/1*RH-ihLouYINXUpRfL4Pc7g.jpeg" width="48" height="48" />
+            </div>
+
+            <div style={{}}>
+              <span style={{}}> huydev </span>
+              <span style={{ fontSize: 16 }}> Apr 4,2020</span>
+            </div>
           </div>
 
-          <div >
-
-            <img style={{ borderRadius: 50 }} alt="Kriss" class="icon" src="https://miro.medium.com/fit/c/96/96/1*RH-ihLouYINXUpRfL4Pc7g.jpeg" width="48" height="48" />
-            <span style={{}}> huydev </span>
-            <span style={{ fontSize: 16 }}> Apr 4,2020</span>
-          </div>
-
           <div>
-            <p dangerouslySetInnerHTML={createMarkup()} style={{ lineHeight: 2.5, textAlign: 'left', margin: 10, fontFamily: 'sans-serif' }} />
+            <p dangerouslySetInnerHTML={{ __html: content }} style={{ lineHeight: 2.5, textAlign: 'left', margin: 10, fontFamily: 'sans-serif' }} />
           </div>
           <div>
-            <NumberList numbers={obj.tag} />
+            <NumberList numbers={tag} />
           </div>
         </div>
       </Container>
@@ -71,6 +73,6 @@ export default function AboutPage(props) {
 export async function getServerSideProps() {
   var rs = await fetch(`http://localhost/getbyid/5f28323b8724bc53eca264b9`);
   var responjson = await rs.json()
-  console.log('rs1111', responjson)
+  // console.log('rs1111', responjson)
   return { props: responjson[0] }
 }

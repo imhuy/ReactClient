ec8e63bbe12e95ea2ce7fd0d37f411cdc1615fcd
Huy <>
2020-09-11 04:57:29 +0700

    xoá code thừa

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,44 +45,12 @@ export default function Home({ allPosts }) {
       <main style={styles.main}>
         <div style={styles.content} >
           <Header />
-          <div style={styles.heroContent}>
-            <Container maxWidth="sm">
-
-              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Album layout
-                            </Typography>
-
-              <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                Something short and leading about the collection below—its contents, the creator, etc.
-                Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-                entirely.
-                            </Typography>
-
-              <div style={styles.heroButtons}>
-                <Grid container spacing={2} justify="center">
-                  <Grid item>
-                    <Button variant="text" color="primary">
-                      Main call to action  </Button>
-
-                  </Grid>
-                  <Grid item>
-                    <Button variant="outlined" color="primary">
-                      Secondary action  </Button>
-
-                  </Grid>
-                </Grid>
-              </div>
-            </Container>
-          </div>
-
           <Container style={styles.cardGrid} >
             {/* End hero unit */}
 
             <Grid container spacing={6}>
               {data.map((item, i) =>
-
-                // <Link as={`/posts/${item.slug}`} key={i} href={{ pathname: '/posts/[slug]', query: { id: item._id } }}>
-                <Link as={`/${item.slug}?postId=${item._id}`} key={i} href={{ pathname: '/[slug]', query: { postId: item._id } }}>
+                <Link key={item._id} as={`/posts/${item.slug}?postId=${item._id}`} href={{ pathname: `/posts/_id`, query: { postId: item._id } }}>
                   <Grid item xs={12} sm={6} md={4}>
 
                     <Paper elevation={0} style={styles.card} >
